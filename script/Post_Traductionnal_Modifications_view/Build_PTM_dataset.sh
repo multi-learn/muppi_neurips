207257d987dd0beb91c48523bbebed7d77a9d794
Kossi KOSSIVI <kossi.kossivi@etu.univ-amu.fr>
2025-05-09 15:57:18 +0200

    Change of the download links and extraction method

--- script/Post_Traductionnal_Modifications_view/Build_PTM_dataset.sh
+++ script/Post_Traductionnal_Modifications_view/Build_PTM_dataset.sh
@@ -4,45 +4,51 @@
 mkdir ../../data/rawData/PTM_data
 cd ../../data/rawData/PTM_data/
 
+# CHANGED: Download links of all dbPTM files
 # Download all dbPTM files
-wget https://awi.cuhk.edu.cn/dbPTM/download/experiment/Phosphorylation.gz
-wget https://awi.cuhk.edu.cn/dbPTM/download/experiment/Acetylation.gz
-wget https://awi.cuhk.edu.cn/dbPTM/download/experiment/Ubiquitination.gz
-wget https://awi.cuhk.edu.cn/dbPTM/download/experiment/Succinylation.gz
-wget https://awi.cuhk.edu.cn/dbPTM/download/experiment/Methylation.gz
-wget https://awi.cuhk.edu.cn/dbPTM/download/experiment/Malonylation.gz
-wget https://awi.cuhk.edu.cn/dbPTM/download/experiment/N-linked\ Glycosylation.gz
-wget https://awi.cuhk.edu.cn/dbPTM/download/experiment/O-linked\ Glycosylation.gz
-wget https://awi.cuhk.edu.cn/dbPTM/download/experiment/Sumoylation.gz
-wget https://awi.cuhk.edu.cn/dbPTM/download/experiment/S-nitrosylation.gz
-wget https://awi.cuhk.edu.cn/dbPTM/download/experiment/Glutathionylation.gz
-wget https://awi.cuhk.edu.cn/dbPTM/download/experiment/Amidation.gz
-wget https://awi.cuhk.edu.cn/dbPTM/download/experiment/Hydroxylation.gz
-wget https://awi.cuhk.edu.cn/dbPTM/download/experiment/Pyrrolidone\ carboxylic\ acid.gz
-wget https://awi.cuhk.edu.cn/dbPTM/download/experiment/Glutarylation.gz
-# wget https://awi.cuhk.edu.cn/dbPTM/download/experiment/Palmitoylation.gz
-wget https://awi.cuhk.edu.cn/dbPTM/download/experiment/Gamma-carboxyglutamic\ acid.gz
-wget https://awi.cuhk.edu.cn/dbPTM/download/experiment/Crotonylation.gz
-wget https://awi.cuhk.edu.cn/dbPTM/download/experiment/Oxidation.gz
-wget https://awi.cuhk.edu.cn/dbPTM/download/experiment/Myristoylation.gz
-wget https://awi.cuhk.edu.cn/dbPTM/download/experiment/C-linked\ Glycosylation.gz
-wget https://awi.cuhk.edu.cn/dbPTM/download/experiment/Sulfation.gz
-wget https://awi.cuhk.edu.cn/dbPTM/download/experiment/Formylation.gz
-wget https://awi.cuhk.edu.cn/dbPTM/download/experiment/Citrullination.gz
-wget https://awi.cuhk.edu.cn/dbPTM/download/experiment/GPI-anchor.gz
-wget https://awi.cuhk.edu.cn/dbPTM/download/experiment/Nitration.gz
-wget https://awi.cuhk.edu.cn/dbPTM/download/experiment/S-diacylglycerol.gz
-wget https://awi.cuhk.edu.cn/dbPTM/download/experiment/Carboxylation.gz
-wget https://awi.cuhk.edu.cn/dbPTM/download/experiment/Lipoylation.gz
-wget https://awi.cuhk.edu.cn/dbPTM/download/experiment/Carbamidation.gz
-wget https://awi.cuhk.edu.cn/dbPTM/download/experiment/Neddylation.gz
-wget https://awi.cuhk.edu.cn/dbPTM/download/experiment/Pyruvate.gz
-wget https://awi.cuhk.edu.cn/dbPTM/download/experiment/S-linked\ Glycosylation.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/Phosphorylation.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/Acetylation.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/Ubiquitination.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/Succinylation.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/Methylation.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/Malonylation.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/N-linked\ Glycosylation.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/O-linked\ Glycosylation.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/Sumoylation.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/S-nitrosylation.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/Glutathionylation.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/Amidation.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/Hydroxylation.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/Pyrrolidone\ carboxylic\ acid.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/Glutarylation.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/S-palmitoylation.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/O-palmitoylation.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/N-palmitoylation.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/Gamma-carboxyglutamic\ acid.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/Crotonylation.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/Oxidation.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/Myristoylation.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/C-linked\ Glycosylation.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/Sulfation.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/Formylation.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/Citrullination.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/GPI-anchor.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/Nitration.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/S-diacylglycerol.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/Carboxylation.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/Lipoylation.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/Carbamidation.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/Neddylation.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/Pyruvate.gz
+wget https://biomics.lab.nycu.edu.tw/dbPTM/download/experiment/S-linked\ Glycosylation.gz
 
 # Unzip all files
 gunzip *
 
-#TODO: Les fichiers télécharger sont des archives TAR dont il faut extraire les fichiers textes
+# CHANGED: How we extract the content of the files
+for file in ./*; do
+  tar -xOf "$file" > "${file}.txt"
+done
 
 # Concatenate them
 cat *.txt > dbPTM_allSpecies.txt
@@ -51,7 +57,7 @@ cat *.txt > dbPTM_allSpecies.txt
 grep HUMAN dbPTM_allSpecies.txt | cut -f 2,4 > dbPTM_human.txt
 
 # Keep only PPI network proteins
-awk -F'\t' 'NR==FNR{c[$1]++;next};c[$2]' ../PPInetwork_proteins.txt dbPTM_human.txt > ../dbPTM_PPIproteins.txt
+awk -F'\t' 'NR==FNR{c[$1]++;next};c[$1]' ../PPInetwork_proteins.txt dbPTM_human.txt > ../dbPTM_PPIproteins.txt
 
 # Remove useless files
 cd ..
