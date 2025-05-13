e6642e39965ddfecf47e56281590d9973e1a3a65
Kossi KOSSIVI <kossi.kossivi@etu.univ-amu.fr>
2025-05-13 12:03:46 +0200

    Update download link

--- script/Phenotype_Ontology_view/Build_PhenotypeOntology_dataset.sh
+++ script/Phenotype_Ontology_view/Build_PhenotypeOntology_dataset.sh
@@ -3,11 +3,8 @@
 cd ../../data/rawData
 
 # Download the HPO ontology and HPO annotation file
-wget http://compbio.charite.de/jenkins/job/hpo.annotations.monthly/lastSuccessfulBuild/artifact/annotation/ALL_SOURCES_ALL_FREQUENCIES_genes_to_phenotype.txt
+wget - 0 HPO_genes_to_phenotype.txt "https://github.com/obophenotype/human-phenotype-ontology/releases/download/v2025-05-06/genes_to_phenotype.txt" HPO_genes_to_phenotype.txt
 wget http://purl.obolibrary.org/obo/hp.obo
-
-# Change the name of the annotation file
-mv ALL_SOURCES_ALL_FREQUENCIES_genes_to_phenotype.txt HPO_genes_to_phenotype.txt
 
 # Run the python script to format the dataset
 cd ../../script/Phenotype_Ontology_view/
