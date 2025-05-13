5b391fe6ca65b08c200fb1300bc6a7a52b588802
Kossi KOSSIVI <kossi.kossivi@etu.univ-amu.fr>
2025-05-13 17:22:00 +0200

    Minors  changes

--- script/Build_all_dataset.sh
+++ script/Build_all_dataset.sh
@@ -1,39 +1,47 @@
 #! /bin/bash
+set -e
 
-# CHANGED: Replacing sh by python for python scripts
+cd PPInetwork_topology_view
+python Build_PPInetwork_topology_dataset.py
+echo "===== 1. Finished PPInetwork_topology_view ======"
 
-cd 3UTR_Complexes_view
+cd ../PPInetwork_Embedding_view
+python Build_PPInetEmbedding_dataset.py
+echo "===== 2. Finished PPInetwork_Embedding_view ====="
+
+cd ../3UTR_Complexes_view
 python Build_3UTR_Complexes_dataset.py
-
-cd ../Cancer_Mutations_view
-sh Build_CancerMutations_dataset.sh
+echo "===== 3. Finished 3UTR_Complexes_view ====="
 
 cd ../Gene_Ontology_view
 sh Build_GeneOntology_datasets.sh
+echo "===== 4. Finished Gene_Ontology_view ====="
 
 cd ../GO_PPInetwork_view
-sh Build_GO-PPInetwork_dataset.sh
+python GO_PPInetwork_settings.py
+python Build_Weighted_Embeddings.py
+echo "===== 5. Finished GO_PPInetwork_view ====="
 
 cd ../Linear_Motifs_view
 sh Build_LinearMotifs_dataset.sh
+echo "===== 6. Finished Linear_Motifs_view ====="
 
 cd ../Phenotype_Ontology_view
 sh Build_PhenotypeOntology_dataset.sh
+echo "===== 7. Finished Phenotype_Ontology_view ====="
 
 cd ../Post_Traductionnal_Modifications_view
 sh Build_PTM_dataset.sh
-
-cd ../PPInetwork_Embedding_view
-sh Build_PPInetEmbedding_dataset.sh
-
-cd ../PPInetwork_topology_view
-python Build_PPInetwork_topology_dataset.py
+echo "===== 8. Finished Post_Traductionnal_Modifications_view ====="
 
 cd ../Protein_Domains_view
 sh Build_ProteinDomains_dataset.sh
+echo "===== 9. Finished Protein_Domains_view ====="
 
 cd ../Subcell_Location_view
 sh Build_SubcellLoc_dataset.sh
+echo "===== 10. Finished Subcell_Location_view ====="
 
 cd ../Tissue_Expression_view
 sh Build_TissueExpression_dataset.sh
+echo "===== 11. Finished Tissue_Expression_view ====="
